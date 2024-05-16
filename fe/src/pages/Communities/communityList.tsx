82418ab4811ff39d7d1bb13be163b8d96638d352
Ryan Adi Prasetyo <ryanadiprasetyo2004@gmail.com>
2024-05-17 00:13:28 +0700

    modal for create community

--- fe/src/pages/Communities/communityList.tsx
+++ fe/src/pages/Communities/communityList.tsx
@@ -50,28 +50,39 @@ const CommunityList = () => {
       })}
       <button
         onClick={handleShowModal}
-        className="absolute bg-gray-500 p-4 rounded-full bottom-4 right-4"
+        className="absolute bg-gray-500 hover:bg-gray-600 p-4 rounded-full bottom-5 right-4"
       >
         <FaPlus />
       </button>
 
       {showModal && (
         <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
-          <div className="bg-white p-6 rounded-lg shadow-lg">
-            <p>Are you sure you want to delete this chat?</p>
-            <div className="flex justify-center mt-4">
-              <button
-                // onClick={confirmDelete}
-                className="bg-gray-500 text-white px-4 py-2 rounded mr-2 hover:bg-red-600 transition-colors duration-300"
-              >
-                Create
-              </button>
-              <button
-                onClick={closeModal}
-                className="bg-red-500 text-white px-4 py-2 rounded hover:bg-gray-600 transition-colors duration-300"
-              >
-                Cancel
-              </button>
+          <div className="bg-white p-8 flex flex-col gap-4 rounded-lg shadow-lg">
+            <div className="flex items-center justify-center">
+              <p>Create Community</p>
+            </div>
+            <div className="flex items-center justify-center flex-col gap-2">
+              <input
+                type="text"
+                placeholder="Community Name"
+                className="border-2 border-gray-300 rounded p-4 mb-4 w-full"
+                // value={name}
+                // onChange={(e) => setName(e.target.value)}
+              />
+              <div className="flex gap-4 justify-center mt-4">
+                <button
+                  onClick={closeModal}
+                  className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 transition-colors duration-300"
+                >
+                  Cancel
+                </button>
+                <button
+                  // onClick={confirmDelete}
+                  className="bg-green-600 text-white px-4 py-2 rounded mr-2 hover:bg-green-700 transition-colors duration-300"
+                >
+                  Create
+                </button>
+              </div>
             </div>
           </div>
         </div>
