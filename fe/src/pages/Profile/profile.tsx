0aa4ae5b9591baa2f3da7d2aff1e043a8b965587
Ryan Adi Prasetyo <ryanadiprasetyo2004@gmail.com>
2024-05-22 00:04:32 +0700

    responsive profile

--- fe/src/pages/Profile/profile.tsx
+++ fe/src/pages/Profile/profile.tsx
@@ -56,62 +56,68 @@ const Profile = () => {
     }
   };
 
-  const setItemUser = () => {
-    if (userData) {
-      localStorage.setItem("user", JSON.stringify(userData));
-    }
-  };
+  // const setItemUser = () => {
+  //   if (userData) {
+  //     localStorage.setItem("user", JSON.stringify(userData));
+  //   }
+  // };
 
   return (
-    <div className="flex flex-col gap-16 bg-main-gradient pt-[160px] px-32 text-color-primary min-h-screen text-2xl font-poppins">
-      <div className="flex justify-between items-start">
-        <div className="flex gap-8">
-          <div>
+    <div className="flex flex-col gap-4 md:gap-16 bg-main-gradient pt-[120px] md:pt-[160px] px-8 md:px-32 text-color-primary min-h-screen text-2xl font-poppins">
+      <div className="flex justify-center  md:justify-between items-start">
+        <div className="flex flex-col md:flex-row md:gap-8">
+          <div className="flex justify-center">
             {image !== null ? (
               <img
                 src={`http://localhost:3000/${image}`}
                 alt="profile picture"
-                className="w-[300px] h-[300px] bg-white shadow-lg object-cover mt-3  border-black  rounded-full"
+                className="w-[200px] h-[200px] md:w-[300px] md:h-[300px] bg-white shadow-lg object-cover mt-3 border-black rounded-full"
               />
             ) : (
-              <div className=" relative w-[250px] h-[250px] shadow-lg object-cover mt-3  border-black rounded-full flex justify-center items-center">
-                <FaUser size={150} className="absolute" />
+              <div className="relative w-[200px] h-[200px] md:w-[250px] md:h-[250px] shadow-lg object-cover mt-3 border-black rounded-full flex justify-center items-center">
+                <FaUser
+                  size={100}
+                  className="absolute md:relative md:w-[150px] md:h-[150px]"
+                />
               </div>
             )}
           </div>
-          <div className="flex flex-col justify-between pt-8">
-            <div className="flex flex-col gap-8">
-              <h5 className="text-[60px] font-bold">{name}</h5>
-              <p>
-                {bio !== "" ? (
-                  <p className="text-[25px]">{bio}</p>
-                ) : (
-                  <p className="text-[25px]">Belum ada bio.</p>
-                )}
+          <div className="flex flex-col md:justify-between justify-center pt-8">
+            <div className="flex flex-col gap-3 md:gap-8 justify-center md:items-start items-center">
+              <h5 className="text-[40px] md:text-[60px] font-bold">{name}</h5>
+              <p className="text-[20px] md:text-[25px]">
+                {bio !== "" ? bio : "Belum ada bio."}
               </p>
             </div>
-            <div className="py-8 flex flex-wrap gap-4 ">
+            <div className="py-8 flex flex-wrap gap-4">
               {(user && user.user_id !== userId) || user === null ? (
                 ""
               ) : (
-                <>
+                <div className="flex gap-5">
                   <Link
                     to={`/profile/update/${userId}`}
-                    onClick={setItemUser}
-                    className="bg-blue-600 text-color-dark font-bold py-3 px-4 text-lg rounded-xl"
+                    className="bg-blue-600 text-color-dark font-bold py-3 px-2 md:px-4 text-lg rounded"
                   >
                     Edit Profile
                   </Link>
-                </>
+                  <Link
+                    to="/video/create"
+                    className="bg-blue-600 text-color-dark font-bold py-3 px-2 md:px-4 text-lg rounded"
+                  >
+                    Create Video
+                  </Link>
+                </div>
               )}
             </div>
           </div>
         </div>
-        <div className="cursor-pointer">
+        <div className="cursor-pointer mt-4 md:mt-0">
           <BsGear size={40} />
         </div>
       </div>
-      <div className="underline font-bold flex ml-16 text-[25px]">Videos</div>
+      <div className="underline font-bold flex ml-8 md:ml-16 text-[20px] md:text-[25px]">
+        Videos
+      </div>
     </div>
   );
 };
