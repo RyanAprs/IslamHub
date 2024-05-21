fc7c0c798b8c1db615c4c5b4269936ebf228b060
Ryan Adi Prasetyo <ryanadiprasetyo2004@gmail.com>
2024-05-21 20:51:40 +0700

    api for create video

--- be/src/routes/video.route.ts
+++ be/src/routes/video.route.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 import { requireUser } from "../middleware/auth";
-import { getVideo } from "../controllers/video.controller";
+import { createVideo, getVideo } from "../controllers/video.controller";
 
 export const VideoRouter: Router = Router();
 
@@ -9,6 +9,6 @@ VideoRouter.get("/", getVideo);
 VideoRouter.get("/search", getVideo);
 VideoRouter.get("/:id", getVideo);
 VideoRouter.get("/:user_id/:user_video_id");
-VideoRouter.post("/");
+VideoRouter.post("/", createVideo);
 VideoRouter.put("/:id");
 VideoRouter.delete("/:id");
