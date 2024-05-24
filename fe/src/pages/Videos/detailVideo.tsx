16d5c60682c23fac4b4578e81920b677000496fb
Ryan Adi Prasetyo <ryanadiprasetyo2004@gmail.com>
2024-05-24 11:30:31 +0700

    detail video ui

--- fe/src/pages/Videos/detailVideo.tsx
+++ fe/src/pages/Videos/detailVideo.tsx
@@ -4,6 +4,7 @@ import { FaPen, FaTrash, FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import BackButton from "../../components/atoms/backButton/backButton";
+import { formatDistanceToNow, parseISO } from "date-fns";
 
 const DetailVideo = () => {
   const { id } = useParams();
@@ -14,6 +15,8 @@ const DetailVideo = () => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [user, setUser] = useState(null);
+  const [createdAt, setCreatedAt] = useState("");
+  const [showFullDescription, setShowFullDescription] = useState(false);
 
   useEffect(() => {
     const getUserDataFromCookie = () => {
@@ -52,6 +55,7 @@ const DetailVideo = () => {
         setTitle(response.data.data.title);
         setName(response.data.data.name);
         setDescription(response.data.data.description);
+        setCreatedAt(response.data.data.createdAt);
       } catch (error) {
         console.error("Error fetching video:", error);
       }
@@ -66,60 +70,82 @@ const DetailVideo = () => {
         <BackButton path="/video" />
       </div>
       <div className="md:px-4 px-1 flex flex-col gap-3">
-        <div className="flex items-center justify-center">
-          {video ? (
-            <video
-              className="md:h-[500px] h-[250px] w-full md:w-[1000px] object-fill  rounded border-gray-400 shadow-md border-[2px]"
-              controls
-            >
-              <source src={video} type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
-          ) : (
-            <p>Loading video...</p>
-          )}
-        </div>
-        <div className="px-4 md:px-24">
-          <h1 className="md:text-3xl text-2xl font-bold">{title}</h1>
-          <p className="text-md">{description}</p>
-
-          <Link
-            to={`/profile/${userVideoId}`}
-            className="flex gap-3 mb-3 items-center py-2 w-max"
-          >
-            <div className="flex gap-1 mb-3 items-center">
-              <p className="rounded-full bg-slate-300 border-black border-[1px]">
-                {userImage ? (
-                  <img
-                    src={`http://localhost:3000/${userImage}`}
-                    alt="user image"
-                    className="rounded-full w-[50px] h-[50px] object-cover"
-                  />
-                ) : (
-                  <FaUser className="text-black rounded-full w-[50px] h-[50px] object-cover" />
+        <div className="flex flex-col gap-3">
+          <div className="flex items-center justify-center">
+            {video ? (
+              <video
+                className="md:h-[500px] h-[250px] w-full md:w-[1000px] object-fill rounded border-gray-400 shadow-md border-[2px]"
+                controls
+              >
+                <source src={video} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            ) : (
+              <p>Loading video...</p>
+            )}
+          </div>
+          <div className=" px-4 md:px-24 flex flex-col gap-5">
+            <div>
+              <div>
+                <div className="md:text-3xl text-2xl font-bold">{title}</div>
+                <div>
+                  {createdAt ? (
+                    <p>{formatDistanceToNow(parseISO(createdAt))} ago</p>
+                  ) : (
+                    <p>Loading date...</p>
+                  )}
+                </div>
+              </div>
+              <div className="text-md">
+                {showFullDescription
+                  ? description
+                  : description.length > 50
+                  ? `${description.slice(0, 20)}...`
+                  : description}
+                {description.length > 50 && (
+                  <button
+                    onClick={() => setShowFullDescription(!showFullDescription)}
+                    className="text-blue-500 ml-2"
+                  >
+                    {showFullDescription
+                      ? "tampilkan lebih sedikit"
+                      : "Lainnya"}
+                  </button>
                 )}
-              </p>
-              <p className="text-lg font-bold uppercase">{name}</p>
+              </div>
             </div>
-          </Link>
+            <Link
+              to={`/profile/${userVideoId}`}
+              className="flex gap-3 mb-3 items-center py-2 w-max"
+            >
+              <div className="flex gap-5 mb-3 items-center">
+                <div className="rounded-full bg-slate-300 border-black border-[1px]">
+                  {userImage ? (
+                    <img
+                      src={`http://localhost:3000/${userImage}`}
+                      alt="user image"
+                      className="rounded-full w-[50px] h-[50px] object-cover"
+                    />
+                  ) : (
+                    <FaUser className="text-black rounded-full w-[50px] h-[50px] object-cover" />
+                  )}
+                </div>
+                <div className="text-lg font-bold uppercase">{name}</div>
+              </div>
+            </Link>
+          </div>
         </div>
       </div>
       {(user && user.user_id !== userVideoId) || !user ? null : (
         <div className="flex justify-center items-center gap-4 p-4">
-          <Link
-            to=""
-            className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition-colors duration-300 flex gap-2 items-center"
-          >
+          <button className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition-colors duration-300 flex gap-2 items-center">
             <FaTrash />
             Delete
-          </Link>
-          <Link
-            to={`/blog/update/${id}`}
-            className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 transition-colors duration-300 flex gap-2 items-center"
-          >
+          </button>
+          <button className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 transition-colors duration-300 flex gap-2 items-center">
             <FaPen />
             Update
-          </Link>
+          </button>
         </div>
       )}
       <div className="p-4 rounded mt-4">{/* <CommentSection /> */}</div>
