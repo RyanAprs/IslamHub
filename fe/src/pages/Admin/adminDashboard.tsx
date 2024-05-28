f42efc664c1e651ad798b8679278da158e8c65ec
Ryan Adi Prasetyo <ryanadiprasetyo2004@gmail.com>
2024-05-28 23:29:44 +0700

    displaying total data

--- fe/src/pages/Admin/adminDashboard.tsx
+++ fe/src/pages/Admin/adminDashboard.tsx
@@ -2,27 +2,37 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 const AdminDashboard = () => {
-  const [data, setData] = useState();
+  const [totalData, setTotalData] = useState(null);
 
-    useEffect(() => {
-      fetchTotalVideo();
-    }, []);
+  useEffect(() => {
+    fetchTotalData();
+  }, []);
 
-    const fetchTotalVideo = async () => {
-      try {
-        const response = await axios.get(
-          "http://localhost:3000/api/v1/admin/count-data"
-        );
-        setData(response.data.data);
-        console.log(response.data.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
+  const fetchTotalData = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:3000/api/v1/admin/count-data"
+      );
+      setTotalData(response.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="bg-main-gradient">
-      <h1>Welcome Admin</h1>
+      {totalData && (
+        <div>
+          {Object.entries(totalData).map(([key, value]) => (
+            <div key={key}>
+              <h2>
+                Total {key.charAt(0).toUpperCase() + key.slice(1)}:{" "}
+                {value.total_data}
+              </h2>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
