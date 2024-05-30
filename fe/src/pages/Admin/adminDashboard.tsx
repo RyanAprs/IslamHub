9c8b306ce882dce16dae7d434b84726baf38c12e
Ryan Adi Prasetyo <ryanadiprasetyo2004@gmail.com>
2024-05-30 21:01:14 +0700

    change ui dashboard admin

--- fe/src/pages/Admin/adminDashboard.tsx
+++ fe/src/pages/Admin/adminDashboard.tsx
@@ -1,8 +1,20 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { FaHandshake, FaUsers, FaVideo } from "react-icons/fa";
 
 const AdminDashboard = () => {
-  const [totalData, setTotalData] = useState(null);
+  const [totalDataUser, setTotalDataUser] = useState({
+    title: "",
+    total_data: 0,
+  });
+  const [totalDataCommunity, setTotalDataCommunity] = useState({
+    title: "",
+    total_data: 0,
+  });
+  const [totalDataVideo, setTotalDataVideo] = useState({
+    title: "",
+    total_data: 0,
+  });
 
   useEffect(() => {
     fetchTotalData();
@@ -13,26 +25,55 @@ const AdminDashboard = () => {
       const response = await axios.get(
         "http://localhost:3000/api/v1/admin/count-data"
       );
-      setTotalData(response.data.data);
+      if (
+        response.data &&
+        response.data.data &&
+        response.data.data.user &&
+        response.data.data.community &&
+        response.data.data.video
+      ) {
+        setTotalDataUser(response.data.data.user);
+        setTotalDataCommunity(response.data.data.community);
+        setTotalDataVideo(response.data.data.video);
+      } else {
+        console.log("Data structure is not as expected");
+      }
     } catch (error) {
       console.log(error);
     }
   };
 
   return (
-    <div className="bg-main-gradient">
-      {totalData && (
-        <div>
-          {Object.entries(totalData).map(([key, value]) => (
-            <div key={key}>
-              <h2>
-                Total {key.charAt(0).toUpperCase() + key.slice(1)}:{" "}
-                {value.total_data}
-              </h2>
-            </div>
-          ))}
+    <div className="">
+      <div className="grid sm:grid-cols-2 md:grid-cols-4 grid-cols-1 gap-6 text-black justify-center p-8">
+        <div className="shadow-lg h-52 cursor-pointer bg-blue-300 p-4 flex  items-center justify-around rounded-xl border-blue-400 border-[2px]">
+          <div className="flex flex-col gap-4 items-center text-xl font-bold">
+            <h2>{totalDataUser.title}</h2>
+            <p>{totalDataUser.total_data}</p>
+          </div>
+          <div>
+            <FaUsers size={90} />
+          </div>
         </div>
-      )}
+        <div className="shadow-lg h-52 cursor-pointer bg-blue-300 p-4 flex  items-center justify-around rounded-xl border-blue-400 border-[2px]">
+          <div className="flex flex-col gap-4 items-center text-xl font-bold">
+            <h2>{totalDataCommunity.title}</h2>
+            <p>{totalDataCommunity.total_data}</p>
+          </div>
+          <div>
+            <FaHandshake size={90} />
+          </div>
+        </div>
+        <div className="shadow-lg h-52 cursor-pointer bg-blue-300 p-4 flex  items-center justify-around rounded-xl border-blue-400 border-[2px]">
+          <div className="flex flex-col gap-4 items-center text-xl font-bold">
+            <h2>{totalDataVideo.title}</h2>
+            <p>{totalDataVideo.total_data}</p>
+          </div>
+          <div>
+            <FaVideo size={90} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
