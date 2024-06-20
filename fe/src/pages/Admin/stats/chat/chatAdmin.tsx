80d922a3da816427df8bc57cc7974414c3484f4b
Ryan Adi Prasetyo <ryanadiprasetyo2004@gmail.com>
2024-06-20 21:44:42 +0700

    fetching data :V

--- fe/src/pages/Admin/stats/chat/chatAdmin.tsx
+++ fe/src/pages/Admin/stats/chat/chatAdmin.tsx
@@ -1,6 +1,20 @@
+import { useEffect } from "react";
 import SideBarAdmin from "../../../../components/atoms/sideBarAdmin/sideBarAdmin";
+import axios from "axios";
 
 const ChatAdmin = () => {
+  useEffect(() => {
+    fetchChats();
+  }, []);
+
+  const fetchChats = async () => {
+    try {
+      const response = await axios.get(`http://localhost:3000/api/v1/chat`);
+      console.log(response.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="flex min-h-screen">
       <SideBarAdmin />
